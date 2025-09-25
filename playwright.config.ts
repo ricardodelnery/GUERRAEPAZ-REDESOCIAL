301cd6522709e2f7c52877c728cd890fc690bfdf
ricardodelnery <ricardodelnery@gmail.com>
2025-09-25 21:47:15 +0000

    test(e2e): webServer timeout 240s

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   outputDir: "test-results",
   reporter: [["html", { open: "never" }]],
   testDir: './tests',
-  timeout: 180_000_000,
+  timeout: 240_000_000_000,
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL || "http://127.0.0.1:3000",
     baseURL,
