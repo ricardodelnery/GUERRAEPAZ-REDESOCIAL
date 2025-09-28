0b76f883c30bf2bb9287ee969cb66b6d32646f57
ricardodelnery <ricardodelnery@gmail.com>
2025-09-28 14:17:44 +0000

    feat: revert to PostgreSQL database configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,9 @@
-// lib/db.ts
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const db = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
