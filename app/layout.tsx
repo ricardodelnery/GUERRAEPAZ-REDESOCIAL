1d069b50af637b481b10fd4ae1965498e4e57f3a
ricardodelnery <ricardodelnery@gmail.com>
2025-09-29 23:56:09 +0000

    fix: corrigir imports de componentes com export nomeado

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import { UiProvider } from './context/UiContext'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import BottomTabBar from './components/BottomTabBar'
-import SearchSheet from './components/SearchSheet'
-import NotifDrawer from './components/NotifDrawer'
+import { BottomTabBar } from './components/BottomTabBar'
+import { SearchSheet } from './components/SearchSheet'
+import { NotifDrawer } from './components/NotifDrawer'
 import './globals.css'
 
 export default function RootLayout({
